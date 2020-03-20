fef95a609673a382de5b46455c9dade39e0ee5df
Igor Cruz <igor_cruz_avelino@hotmail.com>
2020-03-20 16:37:01 -0300

    config controllers and create dashboard_service

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   #Associations
   has_many :series, class_name: "Serie"
-  hasm_many :movies
+  has_many :movies
 
   #Validations
   validates :name, presence: true, uniqueness: true
