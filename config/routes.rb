26f2e1ef709585fce5f05a6a8c81e2faa7210afe
Igor Cruz <igor_cruz_avelino@hotmail.com>
2020-03-20 14:51:24 -0300

    add controllers, config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      #/api/v1/
+      get '/dashboard', to: "dashboards#index", as: 'dashboard'
+      resources :favorites, path: "my_list", only: [:index, :create]
+      delete '/my_list/:type/:id', to: 'favorites#destroy'
+      resources :reviews, only: [:index, :create]
+      resources :searches, path: "search", only: :index
+      resources :series, only: :show
+      resources :movies, only: :show do
+        member do
+          get '/executions', to: 'executions#show'
+          put '/executions', to: 'executions#update'
+        end
+      end
+      resources :recomendations, only: :index
+    end
+  end
 end
