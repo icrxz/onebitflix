baaaf1d806884fb024d15bf0b820a6b6eb9be642
Igor Cruz <igor_cruz_avelino@hotmail.com>
2020-03-22 03:27:30 -0300

    update serializers

--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::MovieSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :title, :description, :episode_number, :thumbnail_key, :featured_thumbnail_key
+  attributes :id, :title, :description, :episode_number, :serie_id
 
   attribute :type do |object|
     'movie'
@@ -19,4 +19,23 @@ class Api::V1::MovieSerializer
       params[:user].favorites.where(favoritable: object).exists?
     end
   end
+
+  attribute :thumbnail_url do |object|
+    AWS_BUCKET.object("thumbnails/#{object.thumbnail_key}").presigned_url(:get, expires_in: 120)
+  end
+     
+  attribute :thumbnail_cover_url do |object|
+    AWS_BUCKET.object("thumbnails/#{object.thumbnail_cover_key}").presigned_url(:get, expires_in: 120)
+  end
+     
+  attribute :featured_thumbnail_url do |object|
+    if object[:featured_thumbnail_key].present?
+      AWS_BUCKET.object("thumbnails/#{object.featured_thumbnail_key}").presigned_url(:get, expires_in: 120)
+    end
+  end
+     
+  attribute :video_url do |object|
+    AWS_BUCKET.object("videos/#{object.video_key}").presigned_url(:get, expires_in: 120)
+  end
+
 end
